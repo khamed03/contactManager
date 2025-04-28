b415dc674889e020c8d0139d4ecc89bdcabb08a0
SK0603 <khalid.hamid2003@gmail.com~>
2025-04-28 12:24:53 +0300

    finished the contact form file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
-import contactForm from './component/css/ContactForm';
+import React, { useState } from 'react';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+import './css/App.css';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const addContact = (contact) => {
+    setContacts([...contacts, { ...contact, id: Date.now() }]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <contactForm/>
-      </header>
+    <div className="app-container">
+      <h1>Contact Manager</h1>
+      <ContactForm addContact={addContact} />
+      <ContactList contacts={contacts} deleteContact={deleteContact} />
     </div>
   );
 }
